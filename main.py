3e68074bdb40638acd0c6ac2850bb5be7052a43d
Cole Swinford <19520329+ColeSwinford@users.noreply.github.com>
2023-05-25 17:05:00 -0400

    added printing to python terminal

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import serial.tools.list_ports
 
 #### Create .txt file ####
 current_time = time.strftime("%m/%d/%Y_%H.%M.%S")
-graph_data = open('data_{}.txt'.format(time.strftime("%m.%d.%Y-%H.%M.%S")), 'w+')
-graph_data.write('Format: rpm, time(seconds)\n\n')
+data = open('data_{}.txt'.format(time.strftime("%m.%d.%Y-%H.%M.%S")), 'w+')
+data.write('Format: rpm, time(seconds)\n')
 
 #### Arduino Data ####
 # Print ports and their descriptions
@@ -15,11 +15,13 @@ for i in ports:
    print(i)
 comPort = input("\nEnter COM port (Ex. com3 or COM3): ")
 arduinoData = serial.Serial(comPort, 115200)
+print('\n\nFormat: rpm, time(seconds)\n')
 while True:
     ### Receive data from Arduino ###
     while arduinoData.inWaiting() == 0: # do nothing while there's no data
         pass # do nothing
 
     dataPacket = arduinoData.readline().decode('ascii') # read the data from the arduino
-    graph_data.write(dataPacket)
-    graph_data.flush()
+    print(dataPacket) # print data read from arduino to terminal
+    data.write(dataPacket)
+    data.flush()
