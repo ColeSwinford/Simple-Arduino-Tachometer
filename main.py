38159b8dc46177ab4af2c2d5a9ef709855e0f6d4
Cole Swinford <site.coleswinford@gmail.com>
2023-05-25 13:46:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import serial
 import time
+import serial.tools.list_ports
 
 #### Create .txt file ####
 current_time = time.strftime("%m/%d/%Y_%H.%M.%S")
@@ -7,7 +8,12 @@ graph_data = open('data_{}.txt'.format(time.strftime("%m.%d.%Y-%H.%M.%S")), 'w+'
 graph_data.write('Format: rpm, time(seconds)\n\n')
 
 #### Arduino Data ####
-comPort = input("Enter COM port (Ex. com3): ")
+# Print ports and their descriptions
+ports = list(serial.tools.list_ports.comports())
+print("Ports:")
+for i in ports:
+   print(i)
+comPort = input("\nEnter COM port (Ex. com3 or COM3): ")
 arduinoData = serial.Serial(comPort, 115200)
 while True:
     ### Receive data from Arduino ###
