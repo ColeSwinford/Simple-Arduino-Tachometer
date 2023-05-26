916493a5ce48880840e8793527b786b2153f42c6
Cole Swinford <19520329+ColeSwinford@users.noreply.github.com>
2023-05-26 13:28:15 -0400

    Automated com port selection, and added dialog for no/multiple ports detected

--- main.py
+++ main.py
@@ -5,17 +5,38 @@ import serial.tools.list_ports
 #### Create .txt file ####
 current_time = time.strftime("%m/%d/%Y_%H.%M.%S")
 data = open('data_{}.txt'.format(time.strftime("%m.%d.%Y-%H.%M.%S")), 'w+')
-data.write('Format: rpm, time(seconds)\n')
+data.write('Format: rpm, time(seconds)')
 
 #### Arduino Data ####
-# Print ports and their descriptions
-ports = list(serial.tools.list_ports.comports())
-print("Ports:")
-for i in ports:
-   print(i)
-comPort = input("\nEnter COM port (Ex. com3 or COM3): ")
+# Select COM port
+def selectPort():
+    # Print ports and their descriptions
+    ports = list(serial.tools.list_ports.comports())
+    print("Ports:")
+    for i in ports:
+        print(i)
+
+    if(len(ports) == 0):
+        print("\nNo COM ports detected. Please connect an Arduino and try again.")
+        againIn = input("Press enter to scan again. Enter any other key to exit.")
+        if(againIn == ""):
+            comPort = selectPort()
+            return comPort
+        else:
+            print("\n\nExiting...")
+            exit()
+    if(len(ports) == 1):
+        comPort = ports[0].device
+        print("\nSelected:", comPort)
+        return comPort
+    else:
+        print("\nMore than one COM port detected. Please enter the COM port you would like to use.")
+        comPort = input("Enter COM port (Ex. com3 or COM3): ")
+        return comPort
+comPort = selectPort()
+
 arduinoData = serial.Serial(comPort, 115200)
-print('\n\nFormat: rpm, time(seconds)\n')
+print('\nFormat: rpm, time(seconds)')
 while True:
     ### Receive data from Arduino ###
     while arduinoData.inWaiting() == 0: # do nothing while there's no data
