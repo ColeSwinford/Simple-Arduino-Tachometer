2fdc9fb3e06f9202eb51305955135826745ff60b
Cole Swinford <19520329+ColeSwinford@users.noreply.github.com>
2023-06-16 11:16:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import serial
 import time
 import serial.tools.list_ports
-
-#### Create .txt file ####
-current_time = time.strftime("%m/%d/%Y_%H.%M.%S")
-data = open('data_{}.txt'.format(time.strftime("%m.%d.%Y-%H.%M.%S")), 'w+')
-data.write('Format: rpm, time(seconds)')
+import threading
 
 #### Arduino Data ####
 # Select COM port
@@ -36,13 +32,34 @@ def selectPort():
 comPort = selectPort()
 
 arduinoData = serial.Serial(comPort, 115200)
-print('\nFormat: rpm, time(seconds)')
-while True:
-    ### Receive data from Arduino ###
-    while arduinoData.inWaiting() == 0: # do nothing while there's no data
-        pass # do nothing
+done = False
 
-    dataPacket = arduinoData.readline().decode('ascii') # read the data from the arduino
-    print(dataPacket) # print data read from arduino to terminal
-    data.write(dataPacket)
-    data.flush()
+def record():
+    #### Create .txt file ####
+    data = open('data_{}.txt'.format(time.strftime("%m.%d.%Y-%H.%M.%S")), 'w+')
+    data.write('Format: rpm, time(seconds)\n')
+
+    # global vars
+    global arduinoData
+    global done
+    # print('\nFormat: rpm, time(seconds)')
+    while not done:
+        ### Receive data from Arduino ###
+        while arduinoData.inWaiting() == 0: # do nothing while there's no data
+            pass # do nothing
+        dataPacket = arduinoData.readline().decode('ascii') # read the data from the arduino
+        # print(dataPacket) # print data read from arduino to terminal
+        data.write(dataPacket)
+        data.flush()
+    done = False
+    return
+
+while True:
+    selection = input("\nPress enter to start recording data.")
+    if(selection == ""):
+        # Create a thread
+        threading.Thread(target=record).start()
+        while done == False:
+            selection = input("Enter 'x' to stop recording and save data.\n")
+            if(selection == "x" or selection == "X"):
+                done = True
